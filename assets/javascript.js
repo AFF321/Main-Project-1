4ae4dd0182a0f94d6f547b0a4bd6ee18eb4df6f9
Edward Arana <Edwardaranadlt@gmail.com>
2021-02-25 13:08:22 -0800

    had to add a commit to pull

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,14 +2,13 @@ var btn = $('.btn')
 var form = $('#searchTextField')
 
 console.log(form.val())
+
 console.log(btn)
 
-function log(){
-  console.log(form.val())
-}
-
 function initialize() {
+  
   var input = document.getElementById('searchTextField');
+  
   new google.maps.places.Autocomplete(input);
 }
 
