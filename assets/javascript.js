549efd8d4b184b7ee9eb2511c7cf263339aef8e4
Andres Fajardo <andresf.f321@yahoo.comgit config user.emailgit config --global user.email andresf.f321@yahoo.com>
2021-02-25 13:07:10 -0800

    cleaned up JS

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,18 +1,10 @@
 var btn = $('.btn')
 var form = $('#searchTextField')
 
-console.log(form.val())
-console.log(btn)
-
-function log(){
-  console.log(form.val())
-}
 
 function initialize() {
   var input = document.getElementById('searchTextField');
   new google.maps.places.Autocomplete(input);
 }
 
-
-
-btn.on('click',log)
+btn.on('click',)
