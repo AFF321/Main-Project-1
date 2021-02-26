f546770602fa841ebb396a9bcfdd152eacc92108
Matthew John <MattJ900@outlook.com>
2021-02-26 11:56:31 -0800

    Weather API Fixed

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,7 +1,8 @@
 var mapEl =$('#mapEl')
-
+var submitBtnEl = $("#submit-btn")
 var btn = $('.btn')
-var form = $('#searchTextField')
+
+var locaitonFieldEl = $('#searchTextField')
 
   function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
@@ -10,36 +11,60 @@ var form = $('#searchTextField')
     });
   }
   
-var weather = $("#weather")
+var results = $("#results")
  function showWeather (){
- $.ajax({ 
-   url:"http://api.weatherapi.com/v1/current.json?key=d1567a95b99d4906ae640957212602&q=Los_angeles"})
- .then(function (data){
-  console.log(data)
-var div = $("<div>").addClass("card")
-var city = $("<div>").addClass("card-title").text("City: " +data.location.name+ "\n Region  "+data.location.region+", Country "+data.location.country+", Lat  "+data.location.lat+", Lon  "+data.location.lon)
-div.append(city)
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    var queryURL = "http://api.weatherapi.com/v1/current.json?key=d1567a95b99d4906ae640957212602&q=" + lat + "," + lng
+    getWeather(queryURL)
+  })
+}
+$("#reset-btn").on("click", function(){
+  results.empty()
+  showWeather()
+} )
 
-var gust = $("<div>").addClass("card-text").text("Gust: " + data.current.gust_mph)
-var humidity = $("<div>").addClass("card-text").text("Humidity: " + data.current.humidity)
-var day = $("<div>").addClass("card-text").text("Day: " + data.current.is_day)
-var tempf = $("<div>").addClass("card-text").text("Temperature:" + data.current.temp_f)
-var uv = $("<div>").addClass("card-text").text("Uv: " + data.current.uv)
-var mph = $("<div>").addClass("card-text").text("WindMph: " + data.current.wind_mph)
 
-div.append(gust)
-div.append(humidity)  
-div.append(day)  
-div.append(tempf)  
-div.append(uv)  
-div.append(mph)  
-weather.append(div)
-})
+
+function getWeather(url) {
+  $.ajax({ 
+    url: url})
+  .then(function (data){
+   console.log(data)
+  var div = $("<div>").addClass("card")
+  var city = $("<div>").addClass("card-title").text("City: " +data.location.name+ "\n Region  "+data.location.region+", Country "+data.location.country+", Lat  "+data.location.lat+", Lon  "+data.location.lon)
+  div.append(city)
+  
+  var gust = $("<div>").addClass("card-text").text("Gust: " + data.current.gust_mph)
+  var humidity = $("<div>").addClass("card-text").text("Humidity: " + data.current.humidity)
+  var day = $("<div>").addClass("card-text").text("Day: " + data.current.is_day)
+  var tempf = $("<div>").addClass("card-text").text("Temperature:" + data.current.temp_f)
+  var uv = $("<div>").addClass("card-text").text("Uv: " + data.current.uv)
+  var mph = $("<div>").addClass("card-text").text("WindMph: " + data.current.wind_mph)
+  var date_epoh = $("<div>").addClass("card-text").text("WindMph: " + data.current.wind_mph)
+  
+  div.append(gust)
+  div.append(humidity)  
+  div.append(day)  
+  div.append(tempf)  
+  div.append(uv)  
+  div.append(mph)  
+  results.append(div)
+  })
+  
 }
 showWeather()
 
 
 
+submitBtnEl.on("click", function(e) {
+  results.empty();
+  e.preventDefault();
+  var value = locaitonFieldEl.val().replace(" ", "_")
+  var queryURL = "http://api.weatherapi.com/v1/current.json?key=d1567a95b99d4906ae640957212602&q=" + value
+  getWeather(queryURL);
+})
 // btn.on('click',)     /DO NOT RUN YET
 
 
