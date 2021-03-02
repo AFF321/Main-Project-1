5c59db05922039399cfc4f2076a9bce4f2b62199
Matthew John <MattJ900@outlook.com>
2021-03-01 17:29:36 -0800

    update on weather api

--- assets/javascript.js
+++ assets/javascript.js
@@ -147,39 +147,42 @@ return marker;
 
  };
  
-var results = $("#results")
+
+// Matthew API
+
+var results = $("#results")                 // Created function 
   function showWeather (){
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function(position) {           // The buildt in geo locater in your brownser  gets your current location  and saves the lat and long into variables then inside of then your API URL
       var lat = position.coords.latitude;
       var lng = position.coords.longitude;
-      var queryURL = "https://api.weatherapi.com/v1/current.json?key=d1567a95b99d4906ae640957212602&q=" + lat + "," + lng
+      var queryURL = "https://api.weatherapi.com/v1/forecast.json?key=d1567a95b99d4906ae640957212602&q=" + lat + "," + lng + "&days=5" //adding the lat and lon variables into our api to locate our exact location
       getWeather(queryURL);
     })
   };
   
-  $("#reset-btn").on("click", function(){
+  $("#reset-btn").on("click", function(){                 //reset button self explantory
     results.empty()
     showWeather()
   })
   
   
   
-  function getWeather(url) {
-    $.ajax({ 
+  function getWeather(url) {          //Calling get weather with our URL that we created in the showWeather function and we use it for our ajax
+    $.ajax({      // ajax = jqery version of fetch
       url: url})
     .then(function (data){
      console.log(data)
     var div = $("<div>").addClass("card borderBx")
     var city = $("<div>").addClass("card-title").text("City: " +data.location.name+ "\n Region  "+data.location.region+", Country "+data.location.country+", Lat  "+data.location.lat+", Lon  "+data.location.lon)
     div.append(city)
-    
+    var forecast = data.forecast.forecastday[0].day.condition.icon
     var gust = $("<div>").addClass("card-text").text("Gust: " + data.current.gust_mph)
     var humidity = $("<div>").addClass("card-text").text("Humidity: " + data.current.humidity)
     var day = $("<div>").addClass("card-text").text("Day: " + data.current.is_day)
     var tempf = $("<div>").addClass("card-text").text("Temperature:" + data.current.temp_f)
     var uv = $("<div>").addClass("card-text").text("Uv: " + data.current.uv)
     var mph = $("<div>").addClass("card-text").text("WindMph: " + data.current.wind_mph)
-    var date_epoh = $("<div>").addClass("card-text").text("WindMph: " + data.current.wind_mph)
+    var image = $("<img>").addClass("forecast-img ").attr( "src","https:" + forecast )
     
     div.append(gust)
     div.append(humidity)  
@@ -187,20 +190,31 @@ var results = $("#results")
     div.append(tempf)  
     div.append(uv)  
     div.append(mph)  
-    results.append(div)
+    div.append(image)
+    results.append(div)  // adding everything into div and put everything inside my  HTML
+
+
+
+
+  
+    
     })
     
   };
   showWeather()
 
-  submitBtnEl.on("click", function(e) {
+  submitBtnEl.on("click", function(e) {        
     results.empty();
     e.preventDefault();
-    var value = locaitonFieldEl.val().replace(" ", "_")
-    var queryURL = "https://api.weatherapi.com/v1/current.json?key=d1567a95b99d4906ae640957212602&q=" + value
+    var value = locaitonFieldEl.val().replace(" ", "_") 
+    var queryURL = "https://api.weatherapi.com/v1/forecast.json?key=d1567a95b99d4906ae640957212602&q=" + value
   
     getWeather(queryURL);
   });
+
+ // how you search a location submit button calls get weather inside function
+ 
+  // Matthew API end
   
   
   function grabLocal(url){
